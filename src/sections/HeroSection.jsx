73cbf21d90bf572218eeae89744bb9f15d273bd7
Haitham Hussien Aleryani <81069558+HaithamHussienAleryani@users.noreply.github.com>
2025-06-25 14:02:10 +0300

    add hero animation

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,31 +1,70 @@
-
-
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+gsap.registerPlugin(SplitText);
 const HeroSection = () => {
-  return (
-    <section className='bg-main-bg'>
-        <div className='hero-container'>
-            <img src="/images/static-img.png" alt="hero-image" className="absolute start-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150 bottom-0" />
-            <div className="hero-content">
-                <div className="overflow-hidden">
-                    <h1 className="hero-title">Freaking delicious</h1>
-                </div>
-                <div style={{
-                     clipPath: 'polygon(50% 0, 50% 0, 50% 100%, 50% 100%)'
-                }} className="hero-text-scroll">
-                    <div className="hero-subtitle">
-  <h1 >Protien + Caffine</h1>
-                    </div>
-                  
-                </div>
-                     <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat asperiores voluptatums </h2>
-                     <div className="hero-button">
-                        <p>check splyt</p>
-                     </div>
-            </div>
-       
-        </div>
-    </section>
-  )
-}
+  useGSAP(() => {
+    const heroTitle = SplitText.create(".hero-title", { type: "chars" });
+    const tl = gsap.timeline({ delay: 1 });
+    tl.to(".hero-content", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
+          ease: "circ.out",
+        },
+        "-=0.5"
+      )
+      .from(
+        heroTitle.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          opacity: 0,
 
-export default HeroSection
+          ease: "power2.out",
+        },
+        "-=0.5"
+      );
+  });
+  return (
+    <section className="bg-main-bg">
+      <div className="hero-container">
+        <img
+          src="/images/static-img.png"
+          alt="hero-image"
+          className="absolute start-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150 top-10"
+        />
+        <div className="hero-content opacity-0">
+          <div className="overflow-hidden">
+            <h1 className="hero-title">Freaking delicious</h1>
+          </div>
+          <div
+            style={{
+              clipPath: "polygon(50% 0%, 50% 0, 50% 100%, 50% 100%)",
+            }}
+            className="hero-text-scroll"
+          >
+            <div className="hero-subtitle">
+              <h1>Protien + Caffine</h1>
+            </div>
+          </div>
+          <h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+            asperiores voluptatums{" "}
+          </h2>
+          <div className="hero-button">
+            <p>check splyt</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
