967670244246f8c913e6d6dac19d3ac5a9a18298
haithamaleryani <haithamaleryani00@gmail.com>
2025-06-29 22:33:30 +0300

    add flavor section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import NavBar from "./components/NavBar";
 
 import HeroSection from "./sections/HeroSection";
-import gsap, { ScrollTrigger, SplitText } from "gsap/all";
+import gsap, { ScrollTrigger } from "gsap/all";
 import MessageSection from "./sections/MessageSection";
+import FalvorSection from "./sections/FalvorSection";
+
 gsap.registerPlugin(ScrollTrigger);
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <NavBar />
       <HeroSection />
       <MessageSection />
+      <FalvorSection/>
       <div className="h-screen"></div>
     </main>
   );
