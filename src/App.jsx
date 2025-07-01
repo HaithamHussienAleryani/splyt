7a9a5a87606246c495cea6d037b6b68a834177ca
Haitham Hussien Aleryani <81069558+HaithamHussienAleryani@users.noreply.github.com>
2025-07-01 15:15:42 +0300

    add testemonial section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import FlavorSection from "./sections/FalvorSection";
 import { useGSAP } from "@gsap/react";
 import NutritionSection from "./sections/NutritionSection";
 import BenefitSection from "./sections/BenefitSection";
+import TestimonialSection from "./sections/TestimonialSection";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -29,6 +30,7 @@ function App() {
           <FlavorSection />
           <NutritionSection />
           <BenefitSection />
+          <TestimonialSection />
           <div className="h-screen"></div>
         </div>
       </div>
